536bbd92ae8335f9e579dae19a152ba893621f92
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-04-19 14:08:13 +0300

    feat(lab2): add Dockerfiles for different environments and implement matrix multiplication endpoint

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,15 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix")
+def matrix_multiply():
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    product = np.dot(matrix_a, matrix_b).tolist()
+
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
